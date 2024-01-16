04e74570ed62d9a0c070da0843c01eb2ce884dc1
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2024-01-16 18:46:52 +0200

    Update addContact.js

--- addContact.js
+++ addContact.js
@@ -1,13 +1,42 @@
-const fs = require("fs").promises;
-const path = require("path");
-const contactsPath = path.join(__dirname, "contacts.json");
 
-const contacts = require("./contacts.json");
+import React, {Component, PropTypes} from 'react';
+import {reduxForm} from 'redux-form';
 
-function addContact()  {
-  contacts.push(filed.value);
-  const dataToAdd = JSON.stringify(contacts);
-  const dataToAddString = `${dataToAdd}`;
-  await fs.writeFile(contactsPath, dataToAddString);
-  return body;
-};
+class ContactForm extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+    fields: PropTypes.object
+  }
+
+  handleSubmit(a) {
+    //do stuff here
+  }
+
+  render() {
+    const {fields: {firstName, lastName, email}, handleSubmit} = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleSubmit)}>
+        <div>
+          <label>First Name</label>
+          <input type="text" placeholder="First Name" {...firstName}/>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type="text" placeholder="Last Name" {...lastName}/>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" placeholder="Email" {...email}/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+// Decorate the form component
+export default reduxForm({
+  form: 'contact', // a unique name for this form
+  fields: ['firstName', 'lastName', 'email']
+})(ContactForm);
